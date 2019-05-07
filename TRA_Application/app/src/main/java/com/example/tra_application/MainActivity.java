e7ec082f7f3922896730e34c1da6c19fcce65a91
unknown <reece.davies@students.plymouth.ac.uk>
2019-05-07 12:11:22 +0100

    Added rest of skils. And changed UI.

--- TRA_Application/app/src/main/java/com/example/tra_application/MainActivity.java
+++ TRA_Application/app/src/main/java/com/example/tra_application/MainActivity.java
@@ -179,86 +179,228 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void checkSkill(TextView textView_fig, TextView textView_tariff, String text)
     {
 
-        if (text.equals("Select skill"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "0", 0.0);
+        switch (text) {
+            case "Select skill":
+                changeScreenValues(textView_fig, textView_tariff, "0", 0.0);
+                break;
+            case "Back drop":
+                changeScreenValues(textView_fig, textView_tariff, "1b", 0.1);
+                break;
+            case "Back somersault (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "4-<", 0.6);
+                break;
+            case "Back somersault (straight)":
+                changeScreenValues(textView_fig, textView_tariff, "4-/", 0.6);
+                break;
+            case "Back somersault (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "4-o", 0.5);
+                break;
+            case "Back somersault to seat (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "4s-<", 0.6);
+                break;
+            case "Back somersault to seat (straight)":
+                changeScreenValues(textView_fig, textView_tariff, "4s-/", 0.6);
+                break;
+            case "Back somersault to seat (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "4s-o", 0.5);
+                break;
+            case "Barani (piked)":
+                changeScreenValues(textView_fig, textView_tariff, "41<", 0.6);
+                break;
+            case "Barani (straight)":
+                changeScreenValues(textView_fig, textView_tariff, "41/", 0.6);
+                break;
+            case "Barani (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "41o", 0.6);
+                break;
+            case "Barani ball out (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "51<", 0.7);
+                break;
+            case "Barani ball out (straight)":
+                changeScreenValues(textView_fig, textView_tariff, "51/", 0.7);
+                break;
+            case "Barani ball out (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "51o", 0.7);
+                break;
+            case "Barani in - back out (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "810<", 1.1);
+                break;
+            case "Barani in - back out (straight)":
+                changeScreenValues(textView_fig, textView_tariff, "810/", 1.1);
+                break;
+            case "Barani in - back out (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "810o", 1.1);
+                break;
+            case "Cody (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "5-<", 0.7);
+                break;
+            case "Cody (straight)":
+                changeScreenValues(textView_fig, textView_tariff, "5-/", 0.7);
+                break;
+            case "Cody (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "5-o", 0.6);
+                break;
+            case "Crash dive":
+                changeScreenValues(textView_fig, textView_tariff, "3b", 0.3);
+                break;
+            case "Double back (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "8--<", 1.2);
+                break;
+            case "Double back (straight)":
+                changeScreenValues(textView_fig, textView_tariff, "8--/", 1.2);
+                break;
+            case "Double back (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "8--o", 1.0);
+                break;
+            case "Double bounce roll (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "8b--<", 1.2);
+                break;
+            case "Double bounce roll (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "8b--o", 1.0);
+                break;
+            case "Front drop":
+                changeScreenValues(textView_fig, textView_tariff, "1f", 0.1);
+                break;
+            case "Front somersault (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "f4-<", 0.6);
+                break;
+            case "Front somersault (straight)":
+                changeScreenValues(textView_fig, textView_tariff, "f4-/", 0.6);
+                break;
+            case "Front somersault (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "f3-o", 0.5);
+                break;
+            case "Full":
+                changeScreenValues(textView_fig, textView_tariff, "42/", 0.7);
+                break;
+            case "Full full (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "822<", 1.6);
+                break;
+            case "Full full (straight)":
+                changeScreenValues(textView_fig, textView_tariff, "822/", 1.6);
+                break;
+            case "Full full (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "822o", 1.4);
+                break;
+            case "Full in - half out (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "821<", 1.5);
+                break;
+            case "Full in - half out (straight)":
+                changeScreenValues(textView_fig, textView_tariff, "821/", 1.5);
+                break;
+            case "Full in - half out (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "821o", 1.3);
+                break;
+            case "Full in - rudi out":
+                changeScreenValues(textView_fig, textView_tariff, "823/", 1.7);
+                break;
+            case "Full twist jump":
+                changeScreenValues(textView_fig, textView_tariff, "02", 0.2);
+                break;
+            case "Half in - half out (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "811<", 1.4);
+                break;
+            case "Half in - half out (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "811o", 1.2);
+                break;
+            case "Half in - rudi out (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "813<", 1.6);
+                break;
+            case "Half in - rudi out (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "813o", 1.4);
+                break;
+            case "Half out (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "801<", 1.3);
+                break;
+            case "Half out (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "801o", 1.1);
+                break;
+            case "Half out ball out (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "901<", 1.4);
+                break;
+            case "Half out ball out (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "901o", 1.2);
+                break;
+            case "Half twist jump":
+                changeScreenValues(textView_fig, textView_tariff, "01", 0.1);
+                break;
+            case "Half twist to back drop":
+                changeScreenValues(textView_fig, textView_tariff, "1b1", 0.2);
+                break;
+            case "Half twist to crash dive":
+                changeScreenValues(textView_fig, textView_tariff, "3b1", 0.4);
+                break;
+            case "Half twist to feet (from B)":
+                changeScreenValues(textView_fig, textView_tariff, "11", 0.2);
+                break;
+            case "Half twist to feet (from S)":
+                changeScreenValues(textView_fig, textView_tariff, "01", 0.1);
+                break;
+            case "Half twist to front drop":
+                changeScreenValues(textView_fig, textView_tariff, "1f1", 0.2);
+                break;
+            case "Half twist to seat drop":
+                changeScreenValues(textView_fig, textView_tariff, "s1", 0.1);
+                break;
+            case "Lazy back":
+                changeScreenValues(textView_fig, textView_tariff, "3f-", 0.3);
+                break;
+            case "Miller":
+                changeScreenValues(textView_fig, textView_tariff, "833/", 1.8);
+                break;
+            case "One and a half twist jump":
+                changeScreenValues(textView_fig, textView_tariff, "03/", 0.3);
+                break;
+            case "One and three (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "7--<", 0.9);
+                break;
+            case "One and three (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "7--o", 0.8);
+                break;
+            case "Pike jump":
+                changeScreenValues(textView_fig, textView_tariff, "<", 0.0);
+                break;
+            case "Randy":
+                changeScreenValues(textView_fig, textView_tariff, "45/", 1.0);
+                break;
+            case "Rudi":
+                changeScreenValues(textView_fig, textView_tariff, "43/", 0.8);
+                break;
+            case "Rudi out (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "803<", 1.5);
+                break;
+            case "Rudi out (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "803o", 1.3);
+                break;
+            case "Seat drop":
+                changeScreenValues(textView_fig, textView_tariff, "0s", 0.0);
+                break;
+            case "Straddle jump":
+                changeScreenValues(textView_fig, textView_tariff, "v", 0.0);
+                break;
+            case "Triff half in - half out (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "12101<", 2.1);
+                break;
+            case "Triff half in - half out (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "12101o", 1.8);
+                break;
+            case "Triff half out (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "12001<", 2.0);
+                break;
+            case "Triff half out (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "12001o", 1.7);
+                break;
+            case "Triff rudi out (pike)":
+                changeScreenValues(textView_fig, textView_tariff, "12003<", 2.2);
+                break;
+            case "Triff rudi out (tuck)":
+                changeScreenValues(textView_fig, textView_tariff, "12003", 1.9);
+                break;
+            case "Tuck jump":
+                changeScreenValues(textView_fig, textView_tariff, "o", 0.0);
+                break;
         }
-        else if(text.equals("Back drop"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "1b", 0.1);
-        }
-        else if(text.equals("Back somersault (pike)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "4-<", 0.6);
-        }
-        else if(text.equals("Back somersault (straight)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "4-/", 0.6);
-        }
-        else if(text.equals("Back somersault (tuck)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "4-o", 0.5);
-        }
-        else if(text.equals("Back somersault to seat (pike)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "4s-<", 0.6);
-        }
-        else if(text.equals("Back somersault to seat (straight)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "4s-/", 0.6);
-        }
-        else if(text.equals("Back somersault to seat (tuck)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "4s-o", 0.5);
-        }
-        else if(text.equals("Barani (piked)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "41<", 0.6);
-        }
-        else if(text.equals("Barani (straight)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "41/", 0.6);
-        }
-        else if(text.equals("Barani (tuck)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "41o", 0.6);
-        }
-        else if(text.equals("Barani ball out (pike)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "51<", 0.7);
-        }
-        else if(text.equals("Barani ball out (straight)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "51/", 0.7);
-        }
-        else if(text.equals("Barani ball out (tuck)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "51o", 0.7);
-        }
-        else if(text.equals("Barani in - back out (pike)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "810<", 1.1);
-        }
-        else if(text.equals("Barani in - back out (straight)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "810/", 1.1);
-        }
-        else if(text.equals("Barani in - back out (tuck)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "810o", 1.1);
-        }
-        else if(text.equals("Cody (pike)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "5-<", 0.7);
-        }
-        else if(text.equals("Cody (straight)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "5-/", 0.7);
-        }
-        else if(text.equals("Cody (tuck)"))
-        {
-            changeScreenValues(textView_fig, textView_tariff, "5-o", 0.6);
-        }
+
 
 
 
